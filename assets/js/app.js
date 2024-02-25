e1e686fba8d8ece8d54a4cf6cf6341da4b879f21
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-02-26 05:22:18 +0600

    js add

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,16 @@
 const navlink = document.querySelectorAll(".nav-links");
 
+const closeBtn = document.getElementById("closeBtn");
+const openBtn = document.getElementById("openBtn");
+closeBtn.addEventListener("click", () => {
+  const vMenu = document.querySelector(".vertical-menu");
+  vMenu.classList.toggle("show");
+});
+openBtn.addEventListener("click", () => {
+  const vMenu = document.querySelector(".vertical-menu");
+  vMenu.classList.toggle("show");
+});
+
 // if location change
 // window.addEventListener("hashchange", (e) => {
 //   const id = e.newURL.split("#")[1];
@@ -51,7 +62,17 @@ window.addEventListener("load", () => {
 function activeLinkColorChange(id) {
   const navlink = document.querySelectorAll(".nav-links");
 
+  console.log(navlink);
   Array.from(navlink[0].children).forEach((child) => {
+    const children = Array.from(child.children)[0];
+
+    if (children.href.split("#")[1] === id) {
+      children.classList.add("active");
+    } else {
+      children.classList.remove("active");
+    }
+  });
+  Array.from(navlink[1].children).forEach((child) => {
     const children = Array.from(child.children)[0];
 
     if (children.href.split("#")[1] === id) {
